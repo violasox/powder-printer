552ce4f18f6a4cf1a598c6867b152755a20d5494
Kaylene Stocking <viola.sox@gmail.com>
2019-03-03 16:54:09 +0000

    Updated pins for pcb

--- motor_driver_test.cpp
+++ motor_driver_test.cpp
@@ -1,22 +1,63 @@
 #include <Arduino.h>
 #include "motor_driver_test.h"
-#define STEP 0
-#define MS1 1
-#define MS2 2
-#define MS3 3
-#define ENABLE 4
-#define DIRECTION 5
+// Enables / disables all motors
+#define ENABLE 2
+// Resets all motors
+#define RESET 3
+// Sleep all motors
+#define SLEEP 6
+
+// X-axis printhead motor
+#define X_M1 7
+#define X_M2 8
+#define X_M3 9
+#define X_STEP 4
+#define X_DIR 10
+
+// Y_axis printhead motor
+#define Y_M1 24
+#define Y_M2 25
+#define Y_M3 26
+#define Y_STEP 5
+#define Y_DIR 27
+
+// Roller motor
+#define ROLL_M1 20
+#define ROLL_M2 19
+#define ROLL_M3 18
+#define ROLL_STEP 23
+#define ROLL_DIR 17
+
+// Print bed motor
+#define BED_M1 16
+#define BED_M2 15
+#define BED_M3 14
+#define BED_STEP 22
+#define BED_DIR 13
+
+// Powder well motor
+#define PDR_M1 39
+#define PDR_M2 38
+#define PDR_M3 37
+#define PDR_STEP 31
+#define PDR_DIR 36
+
+// Other stuff
+#define LED_1 34
+#define LED_2 33
+#define BUTTON 29
 
 void my_main() {
-    pinMode(STEP, OUTPUT);
-    pinMode(MS1, OUTPUT);
-    pinMode(MS2, OUTPUT);
-    pinMode(MS3, OUTPUT);
+    pinMode(X_STEP, OUTPUT);
+    pinMode(X_M1, OUTPUT);
+    pinMode(X_M2, OUTPUT);
+    pinMode(X_M3, OUTPUT);
     pinMode(ENABLE, OUTPUT);
-    pinMode(DIRECTION, OUTPUT);
-    digitalWrite(MS1, HIGH);
-    digitalWrite(MS2, HIGH);
-    digitalWrite(MS3, HIGH);
+    pinMode(X_DIR, OUTPUT);
+
+    digitalWrite(X_M1, HIGH);
+    digitalWrite(X_M2, HIGH);
+    digitalWrite(X_M3, HIGH);
     digitalWrite(ENABLE, LOW);
     Serial.begin(9600);
     delay(1);
@@ -32,16 +73,16 @@ void my_main() {
 void step(Direction dir) {
     if (dir == Direction::FORWARD) {
         Serial.println("Moving forward");
-        digitalWrite(DIRECTION, LOW);
+        digitalWrite(X_DIR, LOW);
     }
     else {
         Serial.println("Moving backward");
-        digitalWrite(DIRECTION, HIGH);
+        digitalWrite(X_DIR, HIGH);
     }
     for (int x = 1; x < 1000; x++) {
-        digitalWrite(STEP, HIGH);
+        digitalWrite(X_STEP, HIGH);
         delay(1);
-        digitalWrite(STEP, LOW);
+        digitalWrite(X_STEP, LOW);
         delay(1);
     }
 }
